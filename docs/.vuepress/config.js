316c611015a5a08feebfd1f18bb541491ff4900d
mattbrailsford <me@mattbrailsford.com>
2018-11-06 15:28:22 +0000

    Import tailwind css files using same @imoprt syntax

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,10 @@ module.exports = {
         sidebar: 'auto',
     },
     stylus: {
-        'include css': true
+        'include css': true,
+        include: [
+            './node_modules/../'      // Shortcut references possible everywhere, e.g. @import 'node_modules/bla'
+        ]
     },
     postcss: {
         plugins: [
